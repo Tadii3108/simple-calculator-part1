250146343654ca08a3817ec1ef0cd805c35ee93f
Tadii3108 <tadiwa.zingoni@umuzi.org>
2019-10-30 20:34:07 +0200

    final: adding two numbers

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -2,13 +2,13 @@ let jasmine = require("jasmine");
 let { add } = require("../calculator.js");
 
 describe("Adding two numbers", function() {
-  it("should return 2", function() {
+  it("should return 0", function() {
     expect(add(0, 0)).toBe(0);
   });
-  it("should return 5", function() {
+  it("should return -2", function() {
     expect(add(-1, -1)).toBe(-2);
   });
-  it("should return -2", function() {
+  it("should return 9", function() {
     expect(add(4, 5)).toBe(9);
   });
 });
