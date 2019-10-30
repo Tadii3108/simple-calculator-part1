c22f8a620e2c9af62b2764f38a3b2727f25a5ece
Tadii3108 <tadiwa.zingoni@umuzi.org>
2019-10-30 21:01:23 +0200

    green: adding many numbers

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -11,7 +11,7 @@ describe("Adding many numbers", function() {
   it("should return 21", function() {
     expect(add(1, 2, 3, 4, 5, 6)).toBe(21);
   });
-  it("should return 50", function() {
-    expect(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)).toBe(50);
+  it("should return 55", function() {
+    expect(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)).toBe(55);
   });
 });
