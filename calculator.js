8b00cf90bae96b8b3e223b0ccfaa0771c5c2311f
Tadii3108 <tadiwa.zingoni@umuzi.org>
2019-10-31 08:34:23 +0200

    green: multiplying many numbers

--- calculator.js
+++ calculator.js
@@ -1,5 +1,10 @@
-function multiply(x,y) {
-  return x * y
+function multiply(...args) {
+  var product = 1;
+
+  for(let k = 0; k < args.length; k++) {
+    product *= args[k];
+  }
+  return product;
 }
 
 module.exports = { multiply }
--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -1,14 +1,14 @@
 let jasmine = require("jasmine");
 let { multiply } = require("../calculator.js");
 
-describe("Multiplying two numbers", function() {
-  it("should return 2", function() {
-    expect(multiply(1, 2)).toBe(2);
+describe("Multiplying many numbers", function() {
+  it("should return 6", function() {
+    expect(multiply(1, 2, 3)).toBe(6);
   });
-  it("should return 10", function() {
-    expect(multiply(5, 2)).toBe(10);
+  it("should return 60", function() {
+    expect(multiply(5, 2, 6)).toBe(60);
   });
-  it("should return 144", function() {
-    expect(multiply(12, 12)).toBe(144);
+  it("should return 288", function() {
+    expect(multiply(12, 12, 1, 2)).toBe(288);
   });
 });
